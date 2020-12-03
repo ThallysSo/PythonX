2a168b50d530249f41456664d7733519ed4b3d88
ThallysSo <thallysdesouza@gmail.com>
2020-12-03 20:41:14 -0300

    commit

--- Capitulo2-Variaveis/Variaveis.py
+++ Capitulo2-Variaveis/Variaveis.py
@@ -2,3 +2,6 @@ nome = "Thallys"
 empresa = 'Foofle'
 qtde_funcionarios = 500
 mediaMensalidade = 856.50
+print(nome + " trabalha na empresa " + empresa)
+print("Possui: ", qtde_funcionarios, " funcionarios.")
+print("A média da mensalidade é de: " + str(mediaMensalidade))
