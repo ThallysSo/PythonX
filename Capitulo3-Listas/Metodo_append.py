96766faf99aca587bde037ba7c5cd5b926fd11a2
ThallysSo <thallysdesouza@gmail.com>
2020-12-05 17:38:51 -0300

    print usando for

--- Capitulo3-Listas/Metodo_append.py
+++ Capitulo3-Listas/Metodo_append.py
@@ -1,10 +1,11 @@
-inventario = []
-resposta = "S"
-while resposta == "S":
+inventario=[]
+resposta="S"
+while resposta=="S":
     inventario.append(input("Equipamento: "))
     inventario.append(float(input("Valor: ")))
-    inventario.append(input("Número Serial: "))
+    inventario.append(int(input("Número Serial: ")))
     inventario.append(input("Departamento: "))
-    resposta = input("Digite \"S\" para continuar: ").upper()
+    resposta=input("Digite \"S\" para continuar: ").upper()
 
-print(inventario)
+for elemento in inventario:
+    print(elemento)
