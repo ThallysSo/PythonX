0158d55bc083fa80e2fc3b6f97bbbd147fa212f1
ThallysSo <thallysdesouza@gmail.com>
2020-12-07 17:26:01 -0300

    Melhorando código

--- Capitulo4_Dicionarios/Dicionario1.py
+++ Capitulo4_Dicionarios/Dicionario1.py
@@ -1,8 +1,15 @@
-usuarios={}
-usuarios={
-    "Chaves": ["Chaves Silva","17/06/1975","Recep_01"],
-    "Quico": ["Enrico Flores","03/06/1976","Raiox_02"],
-    "Quico": ["Enrico Flores","03/06/1976","Raiox_03"]
-    }
+usuarios = {}
+usuarios = {
+    "Chaves": ["Chaves Silva", "17/06/1975", "Recep_01"],
+    "Quico": ["Enrico Flores", "03/06/1976", "Raiox_02"],
+    "Quico": ["Enrico Flores", "03/06/1976", "Raiox_03"]
+}
 # usuarios["Florinda"]=["Florinda Flores", "26/11/2017", "Recep_01"]
 # usuarios["Florinda"]=["Florinda Flores", "26/11/2016", "Recep_01"]
+
+# printar posição
+print(usuarios)
+print("##############========#########")
+print("Dados: ", usuarios.get("Chaves"))
+
+
