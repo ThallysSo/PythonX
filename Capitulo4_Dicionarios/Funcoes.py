06529f84071acbd956f04686d6dc165bd84b1d3c
ThallysSo <thallysdesouza@gmail.com>
2020-12-07 19:02:14 -0300

    commit

--- Capitulo4_Dicionarios/Funcoes.py
+++ Capitulo4_Dicionarios/Funcoes.py
@@ -1,21 +1,24 @@
-from datetime import datetime
-from random import randrange
+import datetime
+import random
+
+
+# -- coding: utf-8 --
 
 def perguntar():
     resposta = input("O que deseja realizar? \n "
                      + "<I> - Para Inserir um usuário \n"
                      + " <P> - Para Pesquisar um usuário \n"
                      + " <E> - Para Excluir um usuário \n"
-                     + " <L> - Para Listar um usuário: ").upper()
+                     + " <L> - Para Listar um usuário \n").upper()
     return resposta
 
 
 def inserir(usuarios):
-    now = datetime.now
-    codigo = str((randrange(1, 101)))
+    now = datetime.datetime.now()
+    codigo = str((random.randrange(1, 101)))
     data = now.strftime("%d/%m/%Y %H:%M:%S")
 
-    arquivo = open('arquivo.txt', 'r') #abrir arquivo
+    arquivo = open('arquivo.txt', 'r')  # abrir arquivo
     conteudo = arquivo.readlines()
 
     print("Seu código de acesso: ", codigo)
@@ -26,29 +29,29 @@ def inserir(usuarios):
     lista = usuarios.get(codigo)
     conteudo.append(str(' [ Nome: ' + lista[0] +
                         ' [ Data: ' + lista[1] +
-                        ' [ Código: ' + lista[2] +
+                        ' [ Código: ' + lista[2] + ']' +
                         '\n'))  # inserindo conteudo
     arquivo = open('arquivo.txt', 'w')  # abre o arquivo novamente (escreve)
     arquivo.writelines(conteudo)  # escreva o conteúdo criado
     arquivo.close()
 
+
 def pesquisar(usuarios, chave):
     lista = usuarios.get(chave)
     if lista is not None:
         print("Nome...........: " + lista[0])
-        print("Último acesso..: " + lista[1])
-        print("Última estação.: " + lista[2])
+        print("Data...........: " + lista[1])
+        print("Código.........: " + lista[2])
 
 
 def excluir(usuarios, chave):
     if usuarios.get(chave) is not None:
         del usuarios[chave]
-    print("Objeto Eliminado")
+    print("Usuário deletado!")
 
 
 def listar(usuarios):
     for chave, valor in usuarios.items():
-        print("Objeto......")
-        print("Login: ", chave)
+        print("Usuários......")
+        print("Códgio de acesso: ", chave)
         print("Dados: ", valor)
-
