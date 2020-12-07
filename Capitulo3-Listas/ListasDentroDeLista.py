fd35fcf5fefd85dfd6bb20e3d5d694ee08ab7ab1
ThallysSo <thallysdesouza@gmail.com>
2020-12-07 17:36:41 -0300

    Melhorando code

--- Capitulo3-Listas/ListasDentroDeLista.py
+++ Capitulo3-Listas/ListasDentroDeLista.py
@@ -1,10 +1,7 @@
-inventario=[]
+inventario = []
 resposta = "S"
 while resposta == "S":
-    equipamento=[input("Equipamento: "),
-              float(input("Valor: ")),
-              int(input("Número Serial: ")),
-              input("Departamento: ")]
+    equipamento = [input("Equipamento: "), float(input("Valor: ")), int(input("Número Serial: ")), input("Departamento: ")]
     inventario.append(equipamento)
     resposta = input("Digite \"S\" para continuar: ").upper()
 
@@ -16,20 +13,20 @@ for elemento in inventario:
 
 busca=input("\nDigite o nome do equipamento que deseja buscar: ")
 for elemento in inventario:
-    if busca==elemento[0]:
+    if busca == elemento[0]:
         print("Valor..: ", elemento[1])
         print("Serial.:", elemento[2])
 
 depreciacao=input("\nDigite o nome do equipamento que será depreciado: ")
 for elemento in inventario:
-    if depreciacao==elemento[0]:
+    if depreciacao == elemento[0]:
         print("Valor antigo: ", elemento[1])
         elemento[1] = elemento[1] * 0.9
         print("Novo valor: ", elemento[1])
 
 serial=int(input("\nDigite o serial do equipamento que será excluído: "))
 for elemento in inventario:
-    if elemento[2]==serial:
+    if elemento[2] == serial:
         inventario.remove(elemento)
 
 for elemento in inventario:
@@ -41,7 +38,7 @@ for elemento in inventario:
 valores=[]
 for elemento in inventario:
     valores.append(elemento[1])
-if len(valores)>0:
+if len(valores) > 0:
     print("O equipamento mais caro custa: ", max(valores))
     print("O equipamento mais barato custa: ", min(valores))
     print("O total de equipamentos é de: ", sum(valores))
