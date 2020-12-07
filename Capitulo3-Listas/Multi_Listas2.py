fe435385b586e3ec9bce407ad6c2a1e25b7a21d2
ThallysSo <thallysdesouza@gmail.com>
2020-12-07 15:37:59 -0300

    depreciação code

--- Capitulo3-Listas/Multi_Listas2.py
+++ Capitulo3-Listas/Multi_Listas2.py
@@ -22,3 +22,10 @@ for indice in range(0, len(equipamentos)):
     if busca == equipamentos[indice]:
         print("Valor..: ", valores[indice])
         print("Serial.:", seriais[indice])
+
+depreciacao = input("\nDigite o nome do equipamento que será depreciado: ")
+for indice in range(0, len(equipamentos)):
+    if depreciacao == equipamentos[indice]:
+        print("Valor antigo: ", valores[indice])
+        valores[indice] = valores[indice] * 0.9
+        print("Novo valor: ", valores[indice])
