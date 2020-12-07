b95fee8de28c5e638eb067228d90407c28e41c93
ThallysSo <thallysdesouza@gmail.com>
2020-12-07 20:49:28 -0300

    Tuplas example

--- Capitulo4_Dicionarios/Tuplas.py
+++ Capitulo4_Dicionarios/Tuplas.py
@@ -7,3 +7,9 @@ while resp == "S":
 print("Exibindo ip´s: ")
 for ip in ips.keys():
     print(ip[0]+"."+ip[1])
+
+print("Exibindo as máquinas que compõem uma mesma rede: ")
+rede = input("Digite os dois primeiros octetos: ")
+for ip, nome in ips.items():
+    if ip[0] == rede:
+        print(nome)
