40d96fb8018302f349d3168f0a5340aebcad6bd9
ThallysSo <thallysdesouza@gmail.com>
2020-12-09 15:59:38 -0300

    melhorando criação e inseção

--- Capitulo4_Dicionarios/Funcoes.py
+++ Capitulo4_Dicionarios/Funcoes.py
@@ -10,7 +10,7 @@ def perguntar():
     resposta = input("O que deseja realizar? \n "
                      + "<I> - Para Inserir um usuário \n"
                      + " <P> - Para Pesquisar um usuário \n"
-                     + " <E> - Para Excluir um usuário \n"
+                     + " <E> - Para Excluir um usuário \n"''
                      + " <L> - Para Listar um usuário \n").upper()
     return resposta
 
