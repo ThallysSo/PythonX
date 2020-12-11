4ba4f12e570832fa51622d017c5126281aa8d315
ThallysSo <thallysdesouza@gmail.com>
2020-12-10 22:57:12 -0300

    persistindo dados em arquivos

--- Capitulo5_ManipulacaoDeArquivos/Inventario.py
+++ Capitulo5_ManipulacaoDeArquivos/Inventario.py
@@ -1,24 +1,12 @@
-inventario = {}
-escolha = int(input('Digite: \n <1> Para registrar ativo \n <2> Para gravar em arquivo \n <3> Para exibir ativos armazenados \n'))
+from Funcoes.FuncoesArquivos import *
 
+inventario = {}
+escolha = menu()
 while escolha > 0 and escolha < 4:
     if escolha == 1:
-        resp = 'S'
-        while resp == 'S':
-            inventario[input('Digite o número patrimonial: ')] = [input('Digite a data da última atualização: '), input('Digite a descrição: '), input('Digite o departamento: ')]
-            resp = input('Digite <S> para continuar. ').upper()
-
+        registrar(inventario)
     elif escolha == 2:
-        with open('inventario.csv', 'a') as estoque:
-            for chave, valor in inventario.items():
-                estoque.write(chave + ' ; ' + valor[0] + ' ; ' + valor[1] + ' ; ' + valor[2] + '\n')
-                print('Gravado com sucesso!')
-
+        gravar(inventario)
     elif escolha == 3:
-        with open('inventario.csv', 'r') as estoque:
-            print(estoque.readlines())
-
-    escolha = int(input('Digite: '
-                        + ' <1> Para registrar ativo \n'
-                        + '         <2> Para gravar em arquivo \n'
-                        + '         <3> Para exibir ativos armazenados \n'))
+        exibir()
+    escolha = menu()
---
+++ Funcoes/FuncoesArquivos.py
@@ -0,0 +1,26 @@
+def menu():
+    escolha = int(input('Digite: \n <1> Para registrar ativo '
+                        '\n <2> Para gravar em arquivo '
+                        '\n <3> Para exibir ativos armazenados \n'))
+    return escolha
+
+
+def registrar(dicionario):
+    resp = 'S'
+    while resp == 'S':
+        dicionario[input('Digite o número patrimonial: ')] = [input('Digite a data da última atualização: '), input('Digite a descrição: '), input('Digite o departamento: ')]
+        resp = input('Digite <S> para continuar. ').upper()
+
+
+def gravar(dicionario):
+    with open('inventario.csv', 'a') as estoque:
+        for chave, valor in dicionario.items():
+            estoque.write(chave + ' ; ' + valor[0] + ' ; ' + valor[1] + ' ; ' + valor[2] + '\n')
+    return print('Gravado com sucesso!')
+
+
+def exibir():
+    with open('inventario.csv', 'r') as estoque:
+        linhas = estoque.readlines()
+    return linhas
+
