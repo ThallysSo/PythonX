0560c1fd8483c602f764a982e9d52c740898808c
ThallysSo <thallysdesouza@gmail.com>
2020-12-10 22:07:23 -0300

    persistindo dados no excel

--- Capitulo5_ManipulacaoDeArquivos/Inventario.py
+++ Capitulo5_ManipulacaoDeArquivos/Inventario.py
@@ -1,15 +1,12 @@
 inventario = {}
 escolha = int(input('Digite: \n <1> Para registrar ativo \n <2> Para gravar em arquivo \n <3> Para exibir ativos armazenados \n'))
 
-while escolha > 1 and escolha < 4:
+while escolha > 0 and escolha < 4:
     if escolha == 1:
         resp = 'S'
         while resp == 'S':
-            inventario[input('Digite o número patrimonial: ')] =\
-                [input('Digite a data da última atualização: '),
-                 input('Digite a descrição: '),
-                 input('Digite o departamento: ')]
-        resp = input('Digite <S> para continuar. ').upper()
+            inventario[input('Digite o número patrimonial: ')] = [input('Digite a data da última atualização: '), input('Digite a descrição: '), input('Digite o departamento: ')]
+            resp = input('Digite <S> para continuar. ').upper()
 
     elif escolha == 2:
         with open('invetario.csv', 'a') as estoque:
