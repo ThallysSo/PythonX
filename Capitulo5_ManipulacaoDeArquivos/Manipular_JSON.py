03c08b578a07a57542f33da86861fa19695e84e1
ThallysSo <thallysdesouza@gmail.com>
2020-12-15 20:41:59 -0300

    modularizando json

--- Capitulo5_ManipulacaoDeArquivos/Manipular_JSON.py
+++ Capitulo5_ManipulacaoDeArquivos/Manipular_JSON.py
@@ -1,29 +1,10 @@
-import json
-import os
+from Funcoes.Funcoes_JSON import *
 
-if os.path.exists('inventario_json.json'):
-    with open('inventario_json.json', 'r') as arq_json:
-        inventario = json.load(arq_json)
-else:
-    inventario = {}
-opcao = int(input("Digite: \n<1> para registrar ativo\n<2> para exibir ativos armazenados\n "))
+inventario = ler_arquivo("inventario_json.json")
+opcao = chamarMenu()
 while opcao > 0 and opcao < 3:
     if opcao == 1:
-        resp = "S"
-        while resp == "S":
-            inventario[input("Digite o número patrimonial: ")] = [
-                input("Digite a data da última atualização: "),
-                input("Digite a descrição: "),
-                input("Digite o departamento: ")]
-            resp = input("Digite <S> para continuar.").upper()
-        with open("inventario_json.json", "w") as arq_json:
-            json.dump(inventario, arq_json)
-        print("JSON gerado!!!!")
+        print(registrar(inventario, "inventario_json.json"))
     elif opcao == 2:
-        with open("inventario_json.json", "r") as arq_json:
-            resultado = json.load(arq_json)
-            for chave, dado in resultado.items():
-                print("Data.........: ", dado[0])
-                print("Descrição....: ", dado[1])
-                print("Departamento.: ", dado[2])
-    opcao = int(input("Digite: \n<1> para registrar ativo\n<2> para exibir ativos armazenados: "))
+        exibir("inventario_json.json")
+    opcao = chamarMenu()
