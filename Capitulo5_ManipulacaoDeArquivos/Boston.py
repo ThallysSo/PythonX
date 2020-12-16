f247920f6ef58ea60b9cd174fb9131eb525efef1
ThallysSo <thallysdesouza@gmail.com>
2020-12-15 23:47:08 -0300

    melhorando o print do mês

--- Capitulo5_ManipulacaoDeArquivos/Boston.py
+++ Capitulo5_ManipulacaoDeArquivos/Boston.py
@@ -1,4 +1,5 @@
 # Qual o total de voos internacionais quue partiram do aeroporto de Logan no ano de 2014
+calendario = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Nov', 'Dez']
 
 with open("economic-indicators.csv", "r") as boston:
     total = 0
@@ -24,7 +25,7 @@ with open("economic-indicators.csv", "r") as boston:
             quantidadeDeVezesQueApareceaData += 1
             quantidadeDePassageirosQuePassaramPeloAeroporto += int(linha.split(',')[2])
     print(quantidadeDeVezesQueApareceaData)
-    print(quantidadeDePassageirosQuePassaramPeloAeroporto)
+    print(quantidadeDePassageirosQuePassaramPeloAeroporto, " pessoas.")
 
 with open("economic-indicators.csv", "r") as boston:
     maiorMediadeDiaria = 0
@@ -34,4 +35,6 @@ with open("economic-indicators.csv", "r") as boston:
         if usuario == int(linha.split(',')[0]) and maiorMediadeDiaria < float(linha.split(',')[5]):
             maiorMediadeDiaria = float(linha.split(',')[5])
             mes = int(linha.split(',')[1])
-    print('Maior média é: ', mes)
+    for m in range(0, len(calendario), 1):
+        if mes == m:
+            print('O mês de', calendario[m-1], 'teve a maior média de preços')
