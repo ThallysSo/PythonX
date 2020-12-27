626a50a9f312627c9e9d1baf136bd0c7f3c9f470
ThallysSo <thallysdesouza@gmail.com>
2020-12-27 20:48:44 -0300

    Melhorando código

--- Capitulo6_BibliotecasExternas/Convert_Geocoder.py
+++ Capitulo6_BibliotecasExternas/Convert_Geocoder.py
@@ -1,6 +1,11 @@
 from pygeocoder import Geocoder
-from Funcoes.Funcoes_JSON import ler_arquivo, gravar_arquivo
+# from Funcoes.Funcoes_JSON import ler_arquivo, gravar_arquivo
 
-dicionario = ler_arquivo("entrada.json")
-saida = {'Local': Geocoder('API').reverse_geocode(-15.8327086, -48.1108383).postal_code}
-gravar_arquivo(saida, "saida.json")
+# dicionario = ler_arquivo("entrada.json")
+
+endereco = 'QNN 22 conjunto I'
+saida = Geocoder('AIzaSyDMNTWTw53ctTCocc5lYjAF7EGLxWTt1Rs').geocode(endereco).postal_code
+print(saida)
+
+
+# gravar_arquivo(saida, "saida.json")
